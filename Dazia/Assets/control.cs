f5384e31e82a564441520ab8a101e5cd24b55deb
bugraince99 <bugra.ince2020@gmail.com>
2024-09-18 03:28:48 +0300

    31

--- Dazia/Assets/control.cs
+++ Dazia/Assets/control.cs
@@ -2,27 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    private Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    public float moveSpeed = 5f; // Hareket hızını belirler
 
     void Update()
     {
-        // Hareket
+        // Girdi al
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
-        rb.AddForce(movement * moveSpeed);
 
-        // Zıplama
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
+        // Hareket yönünü belirle
+        Vector3 move = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
+
+        // Karakteri hareket ettir
+        transform.Translate(move);
     }
 }
